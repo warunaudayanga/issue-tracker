a9b33fe4d2db348f9ae5bd1b731c9e865beb01d2
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2024-07-26 02:43:04 +0530

    Improve issue detail page skeleton

--- app/(pages)/issues/[id]/loading.tsx
+++ app/(pages)/issues/[id]/loading.tsx
@@ -1,28 +1,30 @@
 import { JSX } from "react";
-import { Box, Card, Flex, Skeleton, Text } from "@radix-ui/themes";
+import { Box, Card, Flex, Grid, Skeleton } from "@radix-ui/themes";
 
 const LoadingIssueDetailPage = (): JSX.Element => {
-    const lines = Array.from({ length: 8 }, (_, i) => i);
+    const lines = Array.from({ length: 12 }, (_, i) => i);
 
     return (
-        <Box className="max-w-xl">
-            <Skeleton height="2rem"></Skeleton>
-            <Flex gap="3" mt="2" align="center">
-                <Skeleton loading={true}>
-                    <Text>{"A".repeat(8)}</Text>
-                </Skeleton>
-                <Skeleton>
-                    <Text>{"A".repeat(20)}</Text>
-                </Skeleton>
-            </Flex>
-            <Card className="prose mt-4 space-y-2">
-                {lines.map(i => (
-                    <Skeleton key={i}>
-                        <Text key={i} className="w-full"></Text>
-                    </Skeleton>
-                ))}
-            </Card>
-        </Box>
+        <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+            <Box className="md:col-span-4">
+                <Skeleton height="30px"></Skeleton>
+                <Flex gap="3" mt="2" align="center">
+                    <Skeleton height="20px" width="60px"></Skeleton>
+                    <Skeleton height="24px" width="115px"></Skeleton>
+                </Flex>
+                <Card className="max-w-full mt-4 !flex flex-col gap-4">
+                    {lines.map(line => (
+                        <Skeleton key={line} height="20px" className="w-full"></Skeleton>
+                    ))}
+                </Card>
+            </Box>
+            <Box>
+                <Flex direction="column" gap="4">
+                    <Skeleton height="30px"></Skeleton>
+                    <Skeleton height="30px"></Skeleton>
+                </Flex>
+            </Box>
+        </Grid>
     );
 };
 
