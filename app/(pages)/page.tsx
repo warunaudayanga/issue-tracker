7f6e859e14ca6786cb95ddf3189902c07388ff6b
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2024-08-07 15:40:06 +0530

    Layout dashboard

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -2,11 +2,22 @@ import { JSX } from "react";
 import { IssueSummary, LatestIssues } from "@/app/components";
 import prisma from "@/prisma/client";
 import IssueChart from "@/app/components/IssueChart";
-import { Grid } from "@radix-ui/themes";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home(): Promise<JSX.Element> {
     const open = await prisma.issue.count({ where: { status: "OPEN" } });
     const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
     const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
-    return <Grid columns={{ initial: "1", md: "2" }}></Grid>;
+    const issues = { open, inProgress, closed };
+    return (
+        <Grid columns={{ initial: "1", sm: "2" }} gap="5">
+            <Flex direction="column" gap="5">
+                <IssueSummary {...issues} />
+                <IssueChart {...issues} />
+            </Flex>
+            <Flex>
+                <LatestIssues />
+            </Flex>
+        </Grid>
+    );
 }
