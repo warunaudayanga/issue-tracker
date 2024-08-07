d262aaf78807d31e486ee383a17023bea9db8e0b
Waruna Udayanga <warunaudayangalakruwan@gmail.com>
2024-08-07 15:35:36 +0530

    Build IssueChart component

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,10 +1,12 @@
 import { JSX } from "react";
 import { IssueSummary, LatestIssues } from "@/app/components";
 import prisma from "@/prisma/client";
+import IssueChart from "@/app/components/IssueChart";
+import { Grid } from "@radix-ui/themes";
 
 export default async function Home(): Promise<JSX.Element> {
     const open = await prisma.issue.count({ where: { status: "OPEN" } });
     const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
     const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
-    return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+    return <Grid columns={{ initial: "1", md: "2" }}></Grid>;
 }
